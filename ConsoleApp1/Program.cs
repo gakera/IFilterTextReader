e1f7eeb17b20991ac6c14b40fec8abefb733c74b
Johannes van der Zant <johannes.vanderzant@corpmem.com.au>
2021-02-17 10:49:58 +1000

    Added method to ConsoleApp1 that reads a file using IFilterTextReader. Main method gets documents in directory and feeds them into added method.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IFilterTextReader;
+using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -6,7 +8,50 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var DirectoryPath = @"D:\IFilter Test Documents";
+
+                var filesToProcess = Directory.EnumerateFiles(DirectoryPath);
+
+                foreach (var fileName in filesToProcess)
+                {
+                    var file = new FileInfo(fileName);
+
+                    Console.WriteLine($"Reading {file.Name}");
+
+                    // Try and read the stream from the file
+                    TryReadFile(file);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void TryReadFile(FileInfo file)
+        {
+            var stream = file.OpenRead();
+            FilterReader reader = null;
+            try
+            {
+                FilterReaderOptions filterReaderOptions = new FilterReaderOptions();
+                reader = new FilterReader(stream, file.Extension, filterReaderOptions);
+                var result = reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                reader?.Close();
+                stream?.Close();
+            }
         }
     }
 }
